ff7dc294d052feb40f54d18a74fecd18f6feb782
Alan <alanyd.ph@gmail.com>
2020-09-01 08:43:47 -0300

    Parseado de productos, guardado en lista segun tipo de dato

--- src/EntregableUno/Main.java
+++ src/EntregableUno/Main.java
@@ -1,20 +1,52 @@
 package EntregableUno;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.SQLException;
+import java.util.LinkedList;
+import java.util.List;
+
+import javax.sound.sampled.Line;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
 
 public class Main {
 
 	public static void main(String[] args) {
+
+		//MySql
+		String driver = "com.mysql.cj.jdbc.Driver";
+		String uri = "jdbc.msql://localhost:3306/EntregableUno"; //el entregable uno hay que crearlo en nuestro workbrench local
+		
+		
+		String sentencia = "CREATE TABLE Cliente(" + 
+				"id INT, nombre VARCHAR(500), email VARCHAR(500), PRIMARY KEY(id))";
+		
 		Entidad entidad = new Entidad();
 		Cliente cliente = new Cliente();
+		
+		CSVParser parser;
 		try {
-			cliente.addCliente("pepe", "jaja@hotmail.com");
-			cliente.addCliente("pepe", "jaja@hotmail.com");
-			cliente.addCliente("pepe", "jaja@hotmail.com");
-			cliente.addCliente("pepe", "jaja@hotmail.com");
-		} catch (SQLException e) {
-			e.printStackTrace();
+			List<Producto> productos=new LinkedList<Producto>();
+			parser = CSVFormat.DEFAULT.withHeader().parse(new FileReader("data/productos.csv"));
+			int index = 0;
+			for(CSVRecord row: parser) {
+				productos.add(new Producto(Integer.parseInt(row.get("idProducto")),row.get("nombre"),Float.parseFloat(row.get("valor"))));
+				System.out.println(productos.get(index));
+				index++;
+			}
+		} catch (FileNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
 		}
+				
+		
 	}
 
 }
--- src/EntregableUno/Producto.java
+++ src/EntregableUno/Producto.java
@@ -5,7 +5,39 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class Producto extends Entidad{
+	private int idProducto;
+	private String nombre;
+	private float valor;
 	
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public float getValor() {
+		return valor;
+	}
+	public void setValor(float valor) {
+		this.valor = valor;
+	}
+	public int getIdProducto() {
+		return idProducto;
+	}
+
+	public Producto(int idProducto, String nombre, float valor) {
+		super();
+		this.idProducto = idProducto;
+		this.nombre = nombre;
+		this.valor = valor;
+	}
+	public Producto() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+
 	public void addProducto(int idProducto, String nombre, float valor) throws SQLException {
 		Connection conn = this.driverDB();
 		String insert = "INSERT INTO Producto(idProducto, nombre, valor) VALUES(?,?,?)"; //
@@ -17,4 +49,9 @@ public class Producto extends Entidad{
 		ps.close();
 		conn.commit();
 	}
+	@Override
+	public String toString() {
+		return "Producto [idProducto=" + idProducto + ", nombre=" + nombre + " valor=" + valor + "]";
+	}
+	
 }
